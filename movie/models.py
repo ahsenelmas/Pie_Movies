982a70ee8d3ec5ebe8dad8194e283bd19da066f7
ahsenelmas <149892848+ahsenelmas@users.noreply.github.com>
2024-01-17 06:44:31 +0100

    first commit

---
+++ movie/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Movie(models.Model):
+    image = models.CharField(max_length=2083, null=True)
+    name = models.CharField(max_length=250)
+    type = models.CharField(max_length=250)
+    imdb = models.FloatField(null=True)
+    duration = models.FloatField(null=True)
+    
